41b7bbc895a2fc426e316569973ca0f87a34f160
Ggyumalang <khg2154@naver.com>
2026-03-09 21:42:58 +0900

    코딩 테스트 연습 > 비밀 코드 해독 ---------------
    ---------------

--- src/main/java/exercise/algorithm/programmers/d20260309/exam01.java
+++ src/main/java/exercise/algorithm/programmers/d20260309/exam01.java
@@ -1,53 +1,51 @@
 /**
  * 2025 프로그래머스 코드챌린지 1차 예선 > 비밀 코드 해독
  */
+
 package exercise.algorithm.programmers.d20260309;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class exam01 {
+
     public static void main(String[] args) {
         int n = 10;
         int[][] q = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {3, 7, 8, 9, 10}, {2, 5, 7, 9, 10}, {3, 4, 5, 6, 7}};
-        int [] ans = {2,3,4,3,3};
-        System.out.println(solution(n,q,ans));
+        int[] ans = {2, 3, 4, 3, 3};
+        System.out.println(solution(n, q, ans));
     }
 
     // n 개 중 5 개를 오름차순으로 뽑는다 nP5
-    static Set<int[]> answer;
+    static Set<String> answer;
     static int N;
+
     public static int solution(int n, int[][] q, int[] ans) {
         answer = new HashSet<>();
         N = n;
         int[] selected = new int[5];
-        boolean[] visited = new boolean[n+1];
+        boolean[] visited = new boolean[n + 1];
 
-        permutation(selected, visited, q, ans, 0);
+        permutation(selected, visited, q, ans, 0, 1);
         return answer.size();
     }
 
-    private static void permutation(int[] selected, boolean[] visited, int[][] q, int[] ans, int depth) {
-        if(depth == 5){
+    private static void permutation(int[] selected, boolean[] visited, int[][] q, int[] ans, int depth, int start) {
+        if (depth == 5) {
             Arrays.sort(selected);
-            System.out.println("selected = " + Arrays.toString(selected));
 
-            if(answer.contains(selected)){
-                System.out.println("answer = " + answer);
-                return;
-            }
-
-            if(isMatched(q, ans, selected)){
-                answer.add(selected);
+            if (isMatched(q, ans, selected)) {
+                answer.add(Arrays.stream(selected).mapToObj((x) -> x + "").toString());
             }
             return;
         }
 
-        for (int i = 1; i <= N; i++) {
-            if(!visited[i]){
+        for (int i = start; i <= N; i++) {
+            if (!visited[i]) {
                 visited[i] = true;
                 selected[depth] = i;
-                permutation(selected, visited, q, ans, depth + 1);
+                permutation(selected, visited, q, ans, depth + 1, i);
                 visited[i] = false;
             }
         }
@@ -55,18 +53,18 @@ public class exam01 {
 
     private static boolean isMatched(int[][] q, int[] ans, int[] selected) {
 
-        for(int i = 0; i < q.length; i++){
+        for (int i = 0; i < q.length; i++) {
             int matchedCnt = 0;
             int answerCnt = ans[i];
-            for (int qVal : q[i]) {
-                for (int sVal : selected) {
+            for (int qVal: q[i]) {
+                for (int sVal: selected) {
                     if (qVal == sVal) {
                         matchedCnt++;
                         break;
                     }
                 }
             }
-            if(answerCnt != matchedCnt) {
+            if (answerCnt != matchedCnt) {
                 return false;
             }
         }
