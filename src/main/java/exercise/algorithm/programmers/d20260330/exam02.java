c6c4321b47569df195e178c9c9348d07d7e3aae7
Ggyumalang <khg2154@naver.com>
2026-03-31 09:35:13 +0900

    지마켓 관련 알고리즘

--- src/main/java/exercise/algorithm/programmers/d20260330/exam02.java
+++ src/main/java/exercise/algorithm/programmers/d20260330/exam02.java
@@ -1,4 +1,66 @@
+/**
+ * 상품 검색 랭킹 시스템 (feat.gemini)
+ */
+
 package exercise.algorithm.programmers.d20260330;
 
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class exam02 {
+    public static void main(String[] args) {
+        int[][] products = {{80, 250, 15000}, {82, 50, 20000}, {80, 290, 12000}, {70, 1500, 15000}, {80, 200, 15000}};
+        int k = 3;
+
+        System.out.println(Arrays.toString(solution(products, k)));
+    }
+
+    @ToString
+    static class Product implements Comparable<Product> {
+        int accScore;
+        int accumReviewCount;
+        int price;
+        int idx;
+        int totalScore;
+
+        public Product(int accScore, int accumReviewCount, int price, int idx) {
+            this.accScore = accScore;
+            this.accumReviewCount = accumReviewCount;
+            this.price = price;
+            this.idx = idx;
+            this.totalScore = accScore + (accumReviewCount / 100);
+        }
+
+
+        @Override
+        public int compareTo(Product o) {
+            if(this.totalScore == o.totalScore) {
+                if(this.price == o.price) {
+                    return this.idx - o.idx;
+                }
+                return this.price - o.price;
+            }
+            return o.totalScore - this.totalScore;
+        }
+    }
+
+    public static int[] solution(int[][] products, int k) {
+        PriorityQueue<Product> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < products.length; i++) {
+            queue.offer(new Product(products[i][0], products[i][1], products[i][2], i));
+        }
+
+        int[] answer = new int[k];
+        int idx = 0;
+        while (k > 0 && !queue.isEmpty()) {
+            k--;
+            Product p = queue.poll();
+            answer[idx++] = p.idx;
+        }
+
+        return answer;
+    }
 }
--- src/main/java/exercise/algorithm/programmers/d20260330/exam03.java
+++ src/main/java/exercise/algorithm/programmers/d20260330/exam03.java
@@ -1,4 +1,65 @@
+/**
+ * 장바구니 방치 유저 타겟팅 (문자열 파싱 & 해시) (feat.gemini)
+ */
 package exercise.algorithm.programmers.d20260330;
 
+import java.util.*;
+
 public class exam03 {
+    public static void main(String[] args) {
+        String[] logs = {"alex CART book", "jane CART clothes", "alex PURCHASE book", "jane VIEW book", "tom CART electronics", "tom CART book", "tom PURCHASE book", "alex CART electronics"};
+        System.out.println(Arrays.toString(solution(logs)));
+        System.out.println(Arrays.toString(solution2(logs)));
+    }
+
+    public static String[] solution(String[] logs) {
+        Map<String, String> cartMap = new HashMap<>();
+
+        for (String log : logs) {
+            String[] strs = log.split(" ");
+            System.out.println("strs = " + strs[1]);
+            String key = strs[0] + " " + strs[2];
+            switch (strs[1]) {
+                case "CART":
+                    cartMap.put(key, strs[1]);
+                    break;
+                case "PURCHASE":
+                    cartMap.remove(key);
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("cartMap = " + cartMap);
+        }
+
+        return cartMap.keySet().stream().map(x -> x.split(" ")[0]).sorted().toArray(String[]::new);
+    }
+
+    public static String[] solution2(String[] logs) {
+        Set<String> cartSet = new HashSet<>();
+        Set<String> purchaseSet = new HashSet<>();
+
+        for (String log : logs) {
+            String[] strs = log.split(" ");
+            String key = strs[0] + " " + strs[2];
+            switch (strs[1]) {
+                case "CART":
+                    cartSet.add(key);
+                    break;
+                case "PURCHASE":
+                    purchaseSet.add(key);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        cartSet.removeAll(purchaseSet);
+
+        return cartSet.stream()
+                .map(k -> k.split(" ")[0])
+                .distinct()
+                .sorted()
+                .toArray(String[]::new);
+    }
 }
